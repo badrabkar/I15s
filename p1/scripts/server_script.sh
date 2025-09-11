0951ff60d921e7d8919ef031b62e02897347210d
badrabkar <doublequintal@gmail.com>
2025-09-11 14:37:10 +0100

    feat: Add initial Vagrant configuration
    Initial setup for multi-machine Vagrant environment with:
    - VirtualBox provider with 1024MB memory and 1 CPU
    - Debian 13 base box
    - Two server nodes:
      * tbouzalmS: Main server with static IP 192.168.56.110
      * tbouzalmSW: Server worker with static IP 192.168.56.111
    - Custom provisioning scripts for each server
    - Unique VirtualBox VM names for both nodes"

---
+++ p1/scripts/server_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+echo "You are in the Server" > file
---
+++ p1/scripts/server_worker_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+echo "you are in the SeverWorker" > file
