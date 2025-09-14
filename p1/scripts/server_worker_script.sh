9e2be62ea845f124dfde0f3e5001fc02d321c083
badrabkar <doublequintal@gmail.com>
2025-09-14 11:53:10 +0100

    feat: setup server as tbouzalmS  and server worker as tbouzalmSW - added Flannel CNI (Container Network Interface) to allow pod to pod   communication - set tbouzalmS as server  (controller)with the cluster token set to 12345 - set tbouzalmSW as server worker (agent)and initiate the connection to   tbouzalmS

--- p1/scripts/server_worker_script.sh
+++ p1/scripts/server_worker_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
 
 apt-get update && apt-get upgrade -y
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 -token 12345 -i 192.168.56.111" sh -s -
 echo "you are in the SeverWorker" > file
