6ad8558a4dc4642bc283318f8a418661121e6168
badrabkar <doublequintal@gmail.com>
2025-09-14 15:03:27 +0100

    update: updated the Vagrantfile and removed the scripts - changed the box image from bento/debian13 to generic/alpine319 - replaced the scripts with inline shell commands

--- p1/scripts/server_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x
-apt-get update && apt-get upgrade -y
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--token 12345 -i 192.168.56.110 " sh -s - 
-echo "You are in the Server" > file
